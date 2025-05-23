c8cb006a24286142e57a4ee65b31acfa5ae8abb0
Alit Indrawan <alitindrawan71@gmail.com>
2025-05-23 10:57:41 +0800

    feat: add token validation in scraper controller and update .env.example

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  NotFoundException,
+  Param,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ScraperService } from './scraper.service';
 
 @Controller('scraper')
 export class ScraperController {
   constructor(private readonly scraperService: ScraperService) {}
   @Get('/')
-  async handleScrapeSet(): Promise<string> {
+  async handleScrapeSet(@Headers() headers: Record<string, string>) {
+    const tokenStatic = process.env.TOKEN_STATIC;
+    if (tokenStatic != '' && tokenStatic != headers['x-token-static']) {
+      throw new UnauthorizedException();
+    }
+
     const res = await this.scraperService.scrapeSetList();
     return res;
   }
 
   @Get('/:setCode')
-  async handleScrapeCard(@Param('setCode') setCode: string) {
+  async handleScrapeCard(
+    @Headers() headers: Record<string, string>,
+    @Param('setCode') setCode: string,
+  ) {
+    const tokenStatic = process.env.TOKEN_STATIC;
+    if (tokenStatic != '' && tokenStatic != headers['x-token-static']) {
+      throw new UnauthorizedException();
+    }
+
     const set = await this.scraperService.getSetByCode(setCode);
 
     if (!set) {
